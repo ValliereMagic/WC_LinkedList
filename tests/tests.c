afa38467fcebe9a7376b6c30129b6486dd7ea250
ValliereMagic <ValliereMagic@users.noreply.github.com>
2018-11-02 14:22:05 -0400

    Moved list type out of node, and into the list object itself; prettified source files.

--- tests/tests.c
+++ tests/tests.c
@@ -6,23 +6,29 @@ void test_add_string_element(linked_list_t* list) {
     printf("//Adding some elements to a list\\\\\n");
     
     for (int i = 0; i < 1500; i++) {
-        linked_list_add(list, "HELLO", WC_LINKEDLIST_STRING, 6);
+        
+        linked_list_add(list, "HELLO", 6);
     }
 }
 
 void test_allocate_deallocate(void) {
+    
     printf("//Creating new Linked List\\\\\n");
-    linked_list_t* new_list = linked_list_new();
+    
+    linked_list_t* new_list = linked_list_new(WC_LINKEDLIST_STRING);
 
     test_add_string_element(new_list);
 
     printf("//Freeing newly allocated Linked List\\\\\n");
+    
     linked_list_free(new_list);
 }
 
 int main(void) {
+    
     printf("Hello LinkedList\n");
 
     test_allocate_deallocate();
+    
     return 0;
 }
